5d69a17421d91816c1f709104c3b6d65d0b72ed8
blhack <ryan@blhackbook-pro-2.local>
2016-08-18 23:05:00 -0700

    Support for lighting show programs

--- heliosReceiver.ino
+++ heliosReceiver.ino
@@ -1,25 +1,29 @@
 #define sparkLag 3000
-#define numOfTorches 8
+#define numOfTorches 12
 
 int torchNumber;
 
+int sparkPin = 14;
+
 byte someByte;
-unsigned long torchTimes[8] = {0,0,0,0,0,0,0,0};
-int torchFuel[8] = {2,3,4,5,6,7,8,9};
-int spark[8] = {10,11,12,13,14,15,16,17};
+unsigned long torchTimes[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+int torchFuel[12] = {2,3,4,5,6,7,8,9,10,11,12,13};
+unsigned long showTimes[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+int showPins[10] = {23,24,25,26,27,28,29,47,48,49,50,51,52,53};
+unsigned long sparkOff;
 unsigned long lastKeepAlive;
+int showNumber = 0;
 
 void setup() {
   for (int i=0; i<numOfTorches; i++) {
     pinMode(torchFuel[i], OUTPUT);
-    pinMode(spark[i], OUTPUT);
     digitalWrite(torchFuel[i], LOW);
     delay(100);
     digitalWrite(torchFuel[i], HIGH);
-    digitalWrite(spark[i], LOW);
     delay(100);
-    digitalWrite(spark[i], HIGH);
-  }
+    }
+  pinMode(18, INPUT_PULLUP);
+  pinMode(sparkPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -28,7 +32,11 @@ void readSerial() {
     someByte = Serial.read();
     torchNumber = int(someByte);
     if (torchNumber <= numOfTorches) {
-      torchTimes[torchNumber] = millis() + 100;
+      torchTimes[torchNumber] = millis() + 150;
+    }
+    if (torchNumber >= 100) {
+      showNumber = torchNumber - 100;
+      showTimes[showNumber] = millis() + 500;
     }
   }
 }
@@ -37,6 +45,7 @@ void setTorches() {
   for (int i=0; i<numOfTorches; i++) {
     if (torchTimes[i] > millis()) {
       digitalWrite(torchFuel[i], LOW);
+      sparkOff = millis() + 5000;
     }
     else {
       digitalWrite(torchFuel[i], HIGH);
@@ -45,12 +54,10 @@ void setTorches() {
 }
 
 void setSparks() {
-  for (int i=0; i<numOfTorches; i++) {
-    if ((torchTimes[i] + sparkLag) > millis()) {
-      digitalWrite(spark[i], LOW);
-    } else {
-      digitalWrite(spark[i], HIGH);
-    }
+  if (millis() < sparkOff) {
+    digitalWrite(sparkPin, LOW);
+  } else {
+    digitalWrite(sparkPin, HIGH);
   }
 }
 
@@ -61,10 +68,21 @@ void keepAlive() {
   }
 }
 
+void lightShow() {
+  for (int i=0; i<=14; i++) {
+    if (showTimes[i] > millis()) {
+      digitalWrite(showPins[i], HIGH);
+    } else {
+      digitalWrite(showPins[i], LOW);
+    }
+  }
+}
+
 void loop() {
   readSerial();
   setSparks();
   setTorches();
-  //keepAlive();
+  setLighting();
+  lightShow();
   delay(10);
 }
